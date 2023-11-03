cd9dc24e04428add2c8d5104bb0573938e30ee68
Snus292 <georgiigiblov@gmail.com>
2023-11-03 02:53:28 +0200

    реализованы основые файлы проекта,исправлены импорты

--- src/index.js
+++ src/index.js
@@ -3,6 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reducer from "./reducer"
+import * as actions from "./actions"
+import { bindActionCreators } from 'redux';
+import { createStore } from "redux";
+
+const store = createStore(reducer);
+
+const{dispatch ,subscribe ,getState} = store;
+
+const update = () =>{
+  document.getElementById("counter").textContent = getState().value;
+}
+
+subscribe(update);
+
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args));
+// }
+
+const{inc, dec, rnd} = bindActionCreators(actions, dispatch);
+// const decDispatch = bindActionCreators(dec ,dispatch);
+// const rndDispatch = bindActionCreators(rnd, dispatch);
+
+document.getElementById("inc").addEventListener("click", inc);
+document.getElementById("dec").addEventListener("click", dec);
+document.getElementById("rnd").addEventListener("click", () => {
+  const value = Math.floor(Math.random() * 10);
+  rnd(value);
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
